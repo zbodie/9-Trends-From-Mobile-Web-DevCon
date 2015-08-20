8700deac4233a7049ab7b5aec08407d02bcd035d
zbodie <zach.bodensteiner@gmail.com>
2015-08-19 22:59:08 -0500

    Initial commit of project.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # 9-Trends-From-Mobile-Web-DevCon
+This project is a nodejs powered slideshow for a lunch & learn to be presented about the topics at MWDCON this past July.
+
+## Prerequisites
+This guide assumes you have the following installed:
+* npm
+* nodejs
+* bower
+
+
+## Building the application
+
+After cloning the application, install npm and bower packages.
+
+`npm install`
+`bower install`
+
+## Running the application
+
+Specifying the port is optional.  If it is not specified, the server will start on port 3003.
+
+`node app.js <port>`
+
+## About the application
+
+This application serves up ejs pages as slides for a slideshow.  Left clicking on a slide will advance to the next one, while right clicking on a slide will return to the previous slide.
+
+## Adding a slide
+
+To add a slide, first create an ejs file in the /views/slides folder.  Then, in app.js add an entry to the array of slides specifying the filename and the title to be displayed when the slide is reached.
---
+++ app.js
@@ -0,0 +1,58 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , request = require('request')
+  , partials = require('express-partials')
+  , favicon = require('express-favicon')
+  , bodyParser = require('body-parser')
+  , logger = require('morgan')
+  , port = process.argv[2] || process.env.PORT || 3003;
+  
+var app = express();
+
+  app.set('port', port);
+  app.set('views', __dirname + '/views');
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'bower_components')));
+  app.set('view engine', 'ejs');
+  //app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded());
+  //app.use(express.cookieParser('your secret here'));
+  app.use(partials());
+
+var slides = [{name: 'slides/slide1', title: 'First Slide'},
+			  {name: 'slides/slide2', title: 'Second Slide'}
+			 ];
+
+app.use(function(req, res, next) {
+    res.locals.slideNumber = -1;
+	next();
+});
+
+app.get('/', function(req, res) {
+	res.render('index', {slideTitle: 'Landing'});
+});
+
+app.get('/slide/:id', function(req, res) {
+	var slideId = req.params.id - 1;
+	
+	if(slideId < 0) {
+		res.redirect('/slide/1');
+	} else if(slideId >= slides.length) {
+		res.redirect('/slide/' + slides.length);
+	} else {
+		res.render(slides[slideId].name, {slideTitle: slides[slideId].title, slideNumber: slideId+1});
+	}
+});
+
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
+});
